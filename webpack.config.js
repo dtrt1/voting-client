1eeb650a050c7f0b5863f1f79a9cd6887e390e17
dtrt1 <psyfiiend@gmail.com>
2017-02-26 23:06:26 +0300

    Changed webpack-loaders syntax

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loaders: ['react-hot-loader', 'babel-loader?presets[]=react,presets[]=es2015']
+                loaders: 'react-hot-loader!babel-loader'
             }
         ]
     },
