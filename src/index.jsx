1fd78b45c2cdb8b44c43bd5c218dd7628cf98152
dtrt1 <psyfiiend@gmail.com>
2017-02-26 23:39:34 +0300

    Added voted render and tests

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,6 @@ import Voting from './components/Voting.jsx';
 const pair = ['Trainspotting', '28 Days Later'];
 
 ReactDOM.render(
-    <Voting pair={pair} />,
+    <Voting pair={pair} hasVoted="Trainspotting" />,
     document.getElementById('app')
 );
