7f597c12ff56b92fdf092c9e56f02a888240fab8
dtrt1 <psyfiiend@gmail.com>
2017-03-04 17:52:06 +0300

    Added React-Router, babel-preset-stage-0, App and Results components

---
+++ src/components/Results.jsx
@@ -0,0 +1,28 @@
+/**
+ * Created by DTRT on 04.03.2017.
+ */
+import React from 'react';
+
+export default class extends React.PureComponent {
+    getPair() {
+        return this.props.pair || [];
+    }
+    getVotes(entry) {
+        if (this.props.tally && this.props.tally.has(entry)) {
+            return this.props.tally.get(entry);
+        }
+        return 0;
+    }
+    render() {
+        return <div className="results">
+            {this.getPair().map(entry =>
+                <div key={entry} className="entry">
+                    <h1>{entry}</h1>
+                    <div className="voteCount">
+                        {this.getVotes(entry)}
+                    </div>
+                </div>
+            )}
+        </div>;
+    }
+}
