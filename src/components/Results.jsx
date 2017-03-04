03c88b7d0bd417af09093f130ed66a689f2d8d75
dtrt1 <psyfiiend@gmail.com>
2017-03-04 22:32:44 +0300

    Added Results component and tests

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,6 +2,7 @@
  * Created by DTRT on 04.03.2017.
  */
 import React from 'react';
+import Winner from './Winner';
 
 export default class extends React.PureComponent {
     getPair() {
@@ -14,15 +15,26 @@ export default class extends React.PureComponent {
         return 0;
     }
     render() {
-        return <div className="results">
-            {this.getPair().map(entry =>
-                <div key={entry} className="entry">
-                    <h1>{entry}</h1>
-                    <div className="voteCount">
-                        {this.getVotes(entry)}
-                    </div>
+        return this.props.winner ?
+            <Winner ref="winner" winner={this.props.winner} /> :
+            <div className="results">
+                <div className="tally">
+                    {this.getPair().map(entry =>
+                        <div key={entry} className="entry">
+                            <h1>{entry}</h1>
+                            <div class="voteCount">
+                                {this.getVotes(entry)}
+                            </div>
+                        </div>
+                    )}
                 </div>
-            )}
-        </div>;
+                <div className="management">
+                    <button ref="next"
+                            className="next"
+                            onClick={this.props.next}>
+                        Next
+                    </button>
+                </div>
+            </div>;
     }
 }
