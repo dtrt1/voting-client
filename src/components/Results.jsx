f5804054032c46b437e188e1e0b71c913db22bdf
dtrt1 <psyfiiend@gmail.com>
2017-03-05 16:20:00 +0300

    Added react-redux connect

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,9 +2,10 @@
  * Created by DTRT on 04.03.2017.
  */
 import React from 'react';
+import {connect} from 'react-redux';
 import Winner from './Winner';
 
-export default class extends React.PureComponent {
+export class Results extends React.PureComponent {
     getPair() {
         return this.props.pair || [];
     }
@@ -38,3 +39,13 @@ export default class extends React.PureComponent {
             </div>;
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        pair: state.getIn(['vote', 'pair']),
+        tally: state.getIn(['vote', 'tally']),
+        winner: state.get('winner')
+    }
+}
+
+export const ResultsContainer = connect(mapStateToProps)(Results);
